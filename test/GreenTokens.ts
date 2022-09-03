ac82374a9535715b0720a5a621f3067c871e18ed
Serbrylex <sbryanmadridn@gmail.com>
2022-09-03 07:36:01 -0500

    comments and bignumber

--- test/GreenTokens.ts
+++ test/GreenTokens.ts
@@ -17,35 +17,51 @@ describe("GreenTokens", function () {
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
+    it("Should create correctly the project", async function () {
       const { contract, owner, otherAccount } = await deployContract();
-
+      
+      // Solo usuarios con permisos de CREATOR pueden crear proyectos
+      // solo el ADMIN puede asignar este permiso
       await contract._addRoll(owner.address)
+      // Nombre del proyecto, maxSupply, url del json con la metadata
+      // el json lo podemos generar desde el frontend y subir los archivos automaticamente
+      // a pinata o cualquier proveedor IPFS, igual se valida desde el front haciedo un
+      // y validando que la imagen exista
       await contract.createProject("Green Tokens", 2000, "htts//pinata/metadata.json")
+      // Accedemos al proyecto 0
       const project = await contract.project(0)      
       
+      // el nombre del proyecto es el que seteamos
       expect(project.projectName).to.equal("Green Tokens");
     });
 
-    it("Should set the right unlockTime", async function () {
+    it("Should respect the maxSupply give it", async function () {
       const { contract, owner, otherAccount } = await deployContract();
 
       await contract._addRoll(owner.address)
       await contract.createProject("Green Tokens", 2000, "htts//pinata/metadata.json")
       const project = await contract.project(0)
+
+      // Hasta aquí es lo mismo que arriba
+      
+      // Creamos una instancia del contrato ERC 1155 desplegado dinamicamente
       const baseToken = new ethers.Contract(project.tokens, myJson.abi, ethers.provider)
 
       console.log(baseToken)
 
+      // Obtenemos la uri del json con la metadata
       const uri = await baseToken.uri(0)
       console.log(uri)
 
+      // Obtenemos el balance del owner con respecto a los dos tipos de tokens
       const NFT = await baseToken.functions.balanceOf(owner.address, 0)
-      console.log('NFT', NFT)
+      console.log('NFT', NFT[0])
       const Token = await baseToken.functions.balanceOf(owner.address, 1)
-      console.log('Token', Token)
+      console.log('Token', Token[0])
 
-      expect(project.projectName).to.equal("Green Tokens");
+      // Validamos que el maxSupply sea el establecido
+      expect(Token[0]).to.equal(ethers.BigNumber.from("2000"));
+      expect(NFT[0]).to.equal(ethers.BigNumber.from("1"));
     });
   });
 
